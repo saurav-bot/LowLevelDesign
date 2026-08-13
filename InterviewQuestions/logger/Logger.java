3d8102e4350aacae7376a0d8f38afbdf260cf31f
Saurav <57658456+saurav-bot@users.noreply.github.com>
2026-08-14 00:13:30 +0530

    Handled asynchronous update

--- InterviewQuestions/logger/Logger.java
+++ InterviewQuestions/logger/Logger.java
@@ -4,13 +4,22 @@ import InterviewQuestions.logger.enums.LogLevel;
 import InterviewQuestions.logger.handler.LogHandler;
 import InterviewQuestions.logger.model.LogMessage;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Logger {
     private static final Logger INSTANCE = new Logger();
+    private static final ArrayBlockingQueue<LogMessage> queue =  new ArrayBlockingQueue<>(100000);
 
     private final LogHandler handlerChain;
 
     private Logger() {
         handlerChain = LogHandlerConfiguration.build();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<?> future = executorService.submit(this::handle);
+
     }
 
     public static Logger getInstance() {
@@ -19,7 +28,21 @@ public class Logger {
 
     public void log(LogLevel logLevel, String message) {
         LogMessage msg = new LogMessage(message, logLevel);
-        handlerChain.handel(msg);
+        queue.offer(msg);
+    }
+
+    public void handle() {
+        while (true) {
+            try {
+                LogMessage logMessage = queue.take();
+                handlerChain.handel(logMessage);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+                System.out.println("Error occurred while waiting: " + ex.getMessage());
+                break;
+            }
+
+        }
     }
 
     public void debug(String msg) {
